ff5db3b783ebac5cad4c8c238cdcfbc956a948f3
Geoffrey Emerson <Geoffrey.Emerson.git@gmail.com>
2016-06-13 14:07:12 -0700

    implemented AJAX and localstorage calls

--- scripts/article.js
+++ scripts/article.js
@@ -1,4 +1,4 @@
-function Article (opts) {
+function Article( opts ) {
   this.author = opts.author;
   this.authorUrl = opts.authorUrl;
   this.title = opts.title;
@@ -14,14 +14,14 @@ function Article (opts) {
 // "larger" than any single Article.
 Article.all = [];
 
-Article.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
+Article.prototype.toHtml = function () {
+  var template = Handlebars.compile( $( '#article-template' ).text() );
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt( ( new Date() - new Date( this.publishedOn ) ) / 60 / 60 / 24 / 1000 );
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.body = marked(this.body);
+  this.body = marked( this.body );
 
-  return template(this);
+  return template( this );
 };
 
 // DONE: There are some other functions that also relate to articles across the board, rather than
@@ -31,37 +31,44 @@ Article.prototype.toHtml = function() {
 // DONE: This function will take the rawData, how ever it is provided,
 // and use it to instantiate all the articles. This code is moved from elsewhere, and
 // encapsulated in a simply-named function for clarity.
-Article.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  rawData.forEach(function(ele) {
-    Article.all.push(new Article(ele));
-  });
+Article.loadAll = function ( rawData ) {
+  rawData.sort( function ( a, b ) {
+    return ( new Date( b.publishedOn ) ) - ( new Date( a.publishedOn ) );
+  } );
+  rawData.forEach( function ( ele ) {
+    Article.all.push( new Article( ele ) );
+  } );
 };
 
 // This function will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
-Article.fetchAll = function() {
-  if (localStorage.rawData) {
+Article.fetchAll = function () {
+  if ( localStorage.rawData ) {
     // When rawData is already in localStorage,
     // we can load it by calling the .loadAll function,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(//TODO: What do we pass in here to the .loadAll function?
-    );
-    articleView.someFunctionToCall/*()*/; //TODO: Change this fake method call to the correct one that will render the index page.
+    var storageObject = JSON.parse(localStorage.rawData);
+    Article.loadAll( storageObject ); //DONE: What do we pass in here to the .loadAll function?
+
+    articleView.initIndexPage(); //DONE: Change this fake method call to the correct one that will render the index page.
   } else {
-    // TODO: When we don't already have the rawData in local storage, we need to get it from the JSON file,
+    // DONE: When we don't already have the rawData in local storage, we need to get it from the JSON file,
     //       which simulates data on a remote server. Run live-server or pushstate-server!
     //       Please do NOT browse to your HTML file(s) using a "file:///" link. RUN A SERVER INSTEAD!!
 
     // 1. Retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
 
+    $.getJSON('data/ipsumArticles.json').done(callBackFunction);
     // 2. Store the resulting JSON data with the .loadAll method,
 
-    // 3. Cache the data in localStorage so next time we won't enter this "else" block (avoids hitting the server),
+    function callBackFunction(data) {
+      Article.loadAll(data);
 
-    // 4. Render the index page (perhaps with an articleView method?).
+      // 3. Cache the data in localStorage so next time we won't enter this "else" block (avoids hitting the server),
+      localStorage.rawData = JSON.stringify(Article.all);
+
+      // 4. Render the index page (perhaps with an articleView method?).
+      articleView.initIndexPage();
+    }
   }
 };
